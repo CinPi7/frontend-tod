df5f5ed8b017f4c35dbc1836d6dd47b2b1b70a9b
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-06-08 14:09:20 -0300

    Add files via upload

--- public/scripts/top-slide.js
+++ public/scripts/top-slide.js
@@ -1,12 +1,12 @@
 let slideIndex = 1;
 showSlides(slideIndex);
-//showSecSlides(slideIndex);
+showMainSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function slide(n) {
+function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
@@ -29,5 +29,29 @@ function showSlides(n) {
       }
 
   slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function plusS(n) {
+  showMainSlides(slideIndex += n);
+}
+function currentS(n) {
+  showMainlides(slideIndex = n);
+}
+
+function showMainSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dotz");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "grid";
   dots[slideIndex-1].className += " active";
 }
