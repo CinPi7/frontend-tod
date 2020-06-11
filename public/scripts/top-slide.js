b0c704466d712c3c6ddc95fdd9c3d3e1fa0bd7c2
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-06-11 10:35:04 -0300

    Add files via upload

--- public/scripts/top-slide.js
+++ public/scripts/top-slide.js
@@ -32,26 +32,20 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
+/* Second Gallery */
 function plusS(n) {
     showMainSlides(slideIndex += n);
 }
-function currentS(n) {
-    showMainlides(slideIndex = n);
-}
-
+  
 function showMainSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dotz");
-    
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+
+    let x = document.getElementsByClassName("slide");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "grid";
-    dots[slideIndex-1].className += " active";
+
+    x[slideIndex-1].style.display = "grid";
 }
